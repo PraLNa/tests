54a0ca3edb8bfb5881b75ba90876df292579dfa3
kanae <strausov79@gmail.com>
2025-02-05 16:43:12 +0300

    задачи из телеграмма

--- ex_1.py
+++ ex_1.py
@@ -1,11 +1,31 @@
-numbers = [10, 3, 5, 20, 1]
+# numbers = [10, 3, 5, 20, 1]
+#
+# if not numbers or len(numbers) < 2:
+#     raise ValueError("Список должен содержать как минимум два элемента.")
+#
+# min_num = min(numbers)
+# max_num = max(numbers)
+#
+# max_difference = max_num - min_num
+#
+# print(max_difference)
 
-if not numbers or len(numbers) < 2:
-    raise ValueError("Список должен содержать как минимум два элемента.")
 
-min_num = min(numbers)
-max_num = max(numbers)
+# Максимальная разница
+# напишите программу, которая принимает список чисели возвращает
+# максимальную разницу между двумя любыми элементами списка
+#
+# пример:
+# max_difference([10, 3, 5, 20, 1])
 
-max_difference = max_num - min_num
+def max_difference(numbers):
+    if len(numbers) < 2:
+        raise ValueError("Список должен содержать как минимум два элемента.")
 
-print(max_difference) 
+    max_num = max(numbers)
+    min_num = min(numbers)
+
+    return max_num - min_num
+
+result = max_difference([10, 3, 5, 20, 1])
+print(result)
--- ex_2.py
+++ ex_2.py
@@ -1,8 +1,25 @@
-lst = [1, 2, 3, 4, 5]
-shift = 2
+# lst = [1, 2, 3, 4, 5]
+# shift = 2
+#
+# shift = shift % len(lst)
+#
+# shifted_list = lst[-shift:] + lst[:-shift]
+#
+# print(shifted_list)
+#
+#
+# сдвиг списка
+# напишите программу, которая сдвигает элементы списка на указанное количество позиций вправо.
+# Если сдвиг больше длины списка, он должен повторяться
+#
+# пример:
+# shift_list([1, 2, 3, 4, 5], 2)
 
-shift = shift % len(lst)
+def shift_list(lst, positions):
+    if not lst:
+        return lst
+    positions = positions % len(lst)
+    return lst[-positions:] + lst[:-positions]
 
-shifted_list = lst[-shift:] + lst[:-shift]
-
-print(shifted_list)
+result = shift_list([1, 2, 3, 4, 5], 2)
+print(result)
